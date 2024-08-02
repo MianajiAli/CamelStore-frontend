60901220a9409f5b29d1dbd6f6e144a95df3bc9f
MianajiAli <87234097+MianajiAli@users.noreply.github.com>
2024-08-02 21:38:51 +0330

    Add: Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,14 @@
 const Navbar = () => {
-	return <div className="w-full h-20 bg-zinc-800">Enter</div>;
+	return (
+		<div className="w-full h-20 bg-back2 text-textprimary flex px-[5%] flex-row justify-between items-center">
+			<div className="text-3xl font-bold">Shop.dev</div>
+			<ul className="flex flex-row-reverse gap-5">
+				<li>خانه</li>
+				<li>فروشگاه</li>
+				<li>درباره ما</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Navbar;
